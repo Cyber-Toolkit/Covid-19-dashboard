1db4fd65484738ee4e46b7a4704ed2446a7d03a2
AMANJI <amanjiofficial@gmail.com>
2020-05-20 13:23:06 +0530

    first graph

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-pd = read_csv('covid_19_data.csv')
-print(pd)
+df = pd.read_csv("covid_19_data.csv",index_col = 1)
+ax = df.loc['01/22':'02/22', 'Confirmed'].plot(marker='o', linestyle='-')
+ax.set_ylabel('Confirmed Cases')
+plt.show()
